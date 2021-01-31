fa0bde6449fad400721d2c9c7b46e589f404d077
Sigi <dh1klm@die-jetzis.de>
2021-01-31 19:05:50 +0100

    Update CATCommands.cs
    Anan7000DLE added to ZZFM Radio List

--- Project Files/Source/Console/CAT/CATCommands.cs
+++ Project Files/Source/Console/CAT/CATCommands.cs
@@ -2841,7 +2841,7 @@ namespace Thetis
             else if (radio == "ANAN10" || radio == "ANAN10E")
                 return "0";
 
-            else if (radio == "ANAN100" || radio == "ANAN100B" || radio == "ANAN100D" || radio == "ANAN200D" || radio == "ANAN8000D")
+            else if (radio == "ANAN100" || radio == "ANAN100B" || radio == "ANAN100D" || radio == "ANAN200D" || radio == "ANAN7000DLE" || radio == "ANAN8000D")
                 return "1";
             else
                 return parser.Error1;
